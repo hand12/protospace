f26cbdb6a85cafa12f636098e552c9e4734fe4a4
hand12 <hand12.k@gmail.com>
2016-03-11 15:46:41 +0900

    productの投稿フォームのプロトタイプを作成した。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,21 @@ class ProductsController < ApplicationController
     @product = Product.new
     @product.product_images.build
   end
+
+  def create
+    Product.create(create_params)
+  end
+
+
+  private
+
+  def create_params
+    params.require(:product).permit(
+      :title,
+      :catchcopy,
+      :concept,
+        addresses_attributes: [:id, :image]
+      )
+  end
+
 end
