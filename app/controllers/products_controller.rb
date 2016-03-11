f737de8fb52d24082e9784788b1a69da9ab202af
hand12 <hand12.k@gmail.com>
2016-03-11 17:37:00 +0900

    editとupdateメソッドを追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,15 @@ class ProductsController < ApplicationController
     Product.create(create_params)
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(update_params)
+  end
+
 
   private
 
@@ -17,6 +26,15 @@ class ProductsController < ApplicationController
       :catchcopy,
       :concept,
         product_images_attributes: [:id, :image]
+      )
+  end
+
+  def update_params
+    params.require(:product).permit(
+      :title,
+      :catchcopy,
+      :concept,
+        product_images_attributes: [:id, :image, :_destroy]
       )
   end
 
