a3f1e674113b169431902b566c44da72d3f1aaf5
hand12 <hand12.k@gmail.com>
2016-04-05 17:43:31 +0900

    reject_ifを指定して、product_imageが空欄の時、保存されないようにした.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   belongs_to :user
   has_many :product_images
-  accepts_nested_attributes_for :product_images, allow_destroy: true
+  accepts_nested_attributes_for :product_images, allow_destroy: true, reject_if: proc { |attributes| attributes['image'].blank? }
+
   validates :title, presence: true
 end
--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -2,5 +2,4 @@ class ProductImage < ActiveRecord::Base
   belongs_to :product
   mount_uploader :image, ImageUploader
   enum status: { main: 0, sub: 1}
-  validates :image, presence: true
 end
