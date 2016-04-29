640bab49f12349f2dffefd21b0c5d18e65900ecb
hand12 <hand12.k@gmail.com>
2016-04-29 20:10:15 +0900

    newestを押した時の並び順が逆順になっていたので、並び替えた。

--- app/controllers/products/newest_controller.rb
+++ app/controllers/products/newest_controller.rb
@@ -1,5 +1,5 @@
 class Products::NewestController < ApplicationController
   def index
-    @products = Product.order("created_at ASC").page(params[:page]).per(10)
+    @products = Product.order("created_at DESC").page(params[:page]).per(10)
   end
 end
