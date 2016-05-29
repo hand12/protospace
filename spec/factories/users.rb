dcb075dc6d5c9293fb9cbb2e55ab964053db59ac
hand12 <hand12.k@gmail.com>
2016-05-29 14:38:18 +0900

    Delete Unnecessary description

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,10 +3,6 @@ FactoryGirl.define do
   factory :user do
     name {Faker::Name.name}
     email {Faker::Internet.email}
-    avatar {Faker::Avatar.image("my-own-slug", "50x50", "jpg")}
-    member {Faker::Company.name}
-    profile {Faker::Lorem.sentence}
-    work {Faker::Company.profession}
     password "12345678"
     password_confirmation "12345678"
   end
