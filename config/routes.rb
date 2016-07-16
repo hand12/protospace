0809170b33eda2b98d5d0a80c425ef42fb10f78c
hand12 <hand12.k@gmail.com>
2016-07-16 12:27:47 +0900

    add update to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
  Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 
   namespace :products do
     resources :newest, only: :index
