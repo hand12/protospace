4c31e3443b99141b5a51b2916a08dbb402535f1f
hand12 <hand12.k@gmail.com>
2016-07-18 04:07:01 +0900

    refactoring dirty code

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 describe UsersController do
+
   context "with user login" do
     let!(:user) {create(:user)}
+    let(:params) {{
+      id: user,
+      user: attributes_for(:user, name: 'mage')
+    }}
+    before {
+      login_user
+    }
+
     describe 'GET #show' do
       before :each do
-        get :show, id: user
+        get :show,
+        params
       end
       it "assignd the requested to user" do
         expect(assigns(:user)).to eq user
@@ -13,53 +23,55 @@ describe UsersController do
         expect(response).to render_template :show
       end
     end
+
     describe 'GET #edit' do
       before :each do
-        login_user
+        get :edit,
+        params
       end
       it "assignd the requested to user" do
-        get :edit, id: user
         expect(assigns(:user)).to eq user
       end
       it "renders the :edit templates" do
-        get :edit, id: user
         expect(response).to render_template :edit
       end
     end
+
     describe 'PATCH #update' do
       before :each do
-        login_user
+        patch :update,
+        params
       end
       it "assignd the requested to user" do
-        patch :update, id: user, user: attributes_for(:user)
         expect(assigns(:user)).to eq user
       end
       it "changes user's attributes" do
-        patch :update, id: user, user: attributes_for(:user, name: 'mage')
         user.reload
         expect(user.name).to eq("mage")
       end
       it "redirects root path" do
-        patch :update, id: user, user: attributes_for(:user)
         expect(response).to redirect_to root_path
       end
       it "sends flash messages" do
-        patch :update, id: user, user: attributes_for(:user)
         expect(flash[:notice]).to eq 'Your user infomation was successfully updated'
       end
     end
   end
+
   context "without user login" do
     let!(:user) {create(:user)}
+    let!(:params) {{id: user}}
+
     describe 'GET #edit' do
       it "redirects sign_in page" do
-        get :edit, id: user
+        get :edit, params
         expect(response).to redirect_to new_user_session_path
       end
     end
+
     describe 'PATCH #update' do
       it "redirects sign_in page" do
-        patch :update, id: user
+        patch :update, params
         expect(response).to redirect_to new_user_session_path
       end
     end
