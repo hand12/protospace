bbfc1b556bc47e09849bf3bac737793c7d323bf5
hand12 <hand12.k@gmail.com>
2016-07-19 02:13:11 +0900

    setting product_controllers test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,5 @@
 CarrierWave.configure do |config|
-    config.storage = :fog
-  config.fog_credentials = {
+    config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
@@ -14,5 +13,8 @@ CarrierWave.configure do |config|
     when 'production'
         config.fog_directory  = 'upload-protospace2'
         config.asset_host = 'https://s3.amazonaws.com/upload-protospace2'
+    when 'test'
+        config.storage = :file
+        config.enable_processing = false
     end
 end
