b7e645c50d7cdbaa74fbe49b6f4b529ed173ab43
hand12 <hand12.k@gmail.com>
2016-07-19 11:49:55 +0900

    modify test code to remove unnessecary code

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,18 +3,18 @@ describe UsersController do
 
   context "with user login" do
     let!(:user) {create(:user)}
-    let(:params) {{
-      id: user,
-      user: attributes_for(:user, name: 'mage')
-    }}
-    before {
-      login_user
-    }
+    let(:params) do
+      {
+        id: user,
+        user: attributes_for(:user, name: 'mage')
+      }
+    end
+    before {login_user }
 
     describe 'GET #show' do
       before :each do
         get :show,
-        params
+        id: user
       end
       it "assignd the requested to user" do
         expect(assigns(:user)).to eq user
@@ -27,7 +27,7 @@ describe UsersController do
     describe 'GET #edit' do
       before :each do
         get :edit,
-        params
+        id: user
       end
       it "assignd the requested to user" do
         expect(assigns(:user)).to eq user
@@ -60,18 +60,19 @@ describe UsersController do
 
   context "without user login" do
     let!(:user) {create(:user)}
-    let!(:params) {{id: user}}
 
     describe 'GET #edit' do
       it "redirects sign_in page" do
-        get :edit, params
+        get :edit,
+        id: user
         expect(response).to redirect_to new_user_session_path
       end
     end
 
     describe 'PATCH #update' do
       it "redirects sign_in page" do
-        patch :update, params
+        patch :update,
+        id: user
         expect(response).to redirect_to new_user_session_path
       end
     end
