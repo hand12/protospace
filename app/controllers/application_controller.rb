87610f595b15dac2ddde88ebcb8a1d74be4a5843
hand12 <hand12.k@gmail.com>
2016-07-31 23:30:36 +0900

    update devise settings file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :profile, :member, :email, :password, :password_confirmation, :avatar)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :profile, :member, :email, :password, :password_confirmation, :avatar)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :profile, :member, :email, :password, :password_confirmation, :avatar)}
   end
 end
